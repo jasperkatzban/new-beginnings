3eb262c30ef8fac1aa13f9ec13fe1c799529550f
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2026-07-30 16:09:08 +0200

    template build setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: vitePreprocess(),
     kit: {
+        appDir: 'app',
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
+            fallback: 'app.html'
+        }),
+        paths: {
+            base: ''
+        }
     }
 };
 

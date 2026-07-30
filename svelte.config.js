bf7a5f8368f2841d7426ec3a6df617e992192062
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2026-07-30 16:24:24 +0200

    build setup

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
     kit: {
         appDir: 'app',
         adapter: adapter({
-            fallback: 'app.html'
+            fallback: '404.html'
         }),
         paths: {
             base: ''
